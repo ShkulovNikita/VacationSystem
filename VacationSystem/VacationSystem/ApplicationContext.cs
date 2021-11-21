ed322cb7dda810e4b0e07afe3d53e69ee69924f4
NikitaShkulov <tyuiop_2@mail.ru>
2021-11-21 15:22:23 +0700

    Добавление подключения к БД и логгирования.

---
+++ VacationSystem/VacationSystem/ApplicationContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VacationSystem.Models;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace VacationSystem
+{
+    public class ApplicationContext : DbContext
+    {
+        private readonly StreamWriter logStream = new StreamWriter("logs.txt", true);
+
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+            // установка пути к текущему каталогу
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            // получение конфигурации из файла appsettings.json
+            builder.AddJsonFile("appsettings.json");
+            // создание конфигурации
+            var config = builder.Build();
+            // получение строки подключения
+            string connectionString = config.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseMySql(
+                connectionString,
+                new MySqlServerVersion(new Version(5, 7, 35))
+                );
+            optionsBuilder.LogTo(logStream.WriteLine);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            logStream.Dispose();
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            await base.DisposeAsync();
+            await logStream.DisposeAsync();
+        }
+    }
+}
